f2745280018006c5d6abd0f4b734710a144a6ddf
NLMDang22520190 <22520190@gm.uit.edu.vn>
2024-11-07 11:41:27 +0700

    Enhance LeftSideBar layout with additional separators and adjust padding in Home page

--- src/components/LeftSideBar/LeftSideBar.jsx
+++ src/components/LeftSideBar/LeftSideBar.jsx
@@ -22,10 +22,11 @@ const LeftSideBar = () => {
 
           <span>Home</span>
         </Link>
+        <hr class="h-0.5 border-t-0 bg-gray-200 dark:bg-white/10" />
         {/* Questions */}
         <Link
           to="/questions"
-          className="flex items-center space-x-2 text-gray-700 hover:text-gray-900"
+          className="flex items-center space-x-2  text-gray-700 hover:text-gray-900"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -42,6 +43,8 @@ const LeftSideBar = () => {
 
           <span>Questions</span>
         </Link>
+        <hr class="h-0.5 border-t-0 bg-gray-200 dark:bg-white/10" />
+
         {/* Tags */}
         <Link
           to="/tags"
@@ -62,6 +65,8 @@ const LeftSideBar = () => {
 
           <span>Tags</span>
         </Link>
+        <hr class="h-0.5 border-t-0 bg-gray-200 dark:bg-white/10" />
+
         {/* Users */}
         <Link
           to="/users"
@@ -75,7 +80,6 @@ const LeftSideBar = () => {
           >
             <path d="M4.5 6.375a4.125 4.125 0 1 1 8.25 0 4.125 4.125 0 0 1-8.25 0ZM14.25 8.625a3.375 3.375 0 1 1 6.75 0 3.375 3.375 0 0 1-6.75 0ZM1.5 19.125a7.125 7.125 0 0 1 14.25 0v.003l-.001.119a.75.75 0 0 1-.363.63 13.067 13.067 0 0 1-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 0 1-.364-.63l-.001-.122ZM17.25 19.128l-.001.144a2.25 2.25 0 0 1-.233.96 10.088 10.088 0 0 0 5.06-1.01.75.75 0 0 0 .42-.643 4.875 4.875 0 0 0-6.957-4.611 8.586 8.586 0 0 1 1.71 5.157v.003Z" />
           </svg>
-
           <span>Users</span>
         </Link>
       </nav>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,7 +6,7 @@ import RightSideBar from "../../components/RightSideBar/RightSideBar";
 const Home = () => {
   return (
     <div className="container mx-auto flex mt-4 h-screen">
-      <aside className="w-1/4 bg-white p-4 shadow-2xl sidebar">
+      <aside className="w-1/4 bg-white p-8  shadow-2xl sidebar">
         <LeftSideBar></LeftSideBar>
       </aside>
       <main className="w-2/4 bg-white p-4 shadow-xl ml-4">
